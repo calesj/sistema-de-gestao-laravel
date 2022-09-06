2478419d4d9458e5ae63854bef0b706bec615509
Cales <86434489+calesj@users.noreply.github.com>
2022-09-06 16:06:06 -0300

    add SiteContatoFactory

---
+++ database/factories/SiteContatoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+//Factory é utilizado para a criação de registros aleatorios no banco de dados
+//nesse caso utilizaremos na tabela SiteContato
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SiteContato;
+use Faker\Generator as Faker;
+
+$factory->define(SiteContato::class, function (Faker $faker) {
+    //definindo os campos que irão ser criados no registro
+    return [
+        'nome' => $faker->name,
+
+        //vai criar telefones no banco de dados com a estrutura parecida com a nossa br
+        'telefone' => $faker->tollFreePhoneNumber,
+
+        'email' => $faker->unique()->email,
+
+        //vai gerar numeros aleatorios entre 1 e 3
+        'motivo_contato' => $faker->numberBetween(1,3),
+
+        'mensagem' => $faker->text(200)
+    ];
+});
