2da22e91fca793664b875a7332cafaa525c9950f
Cales <86434489+calesj@users.noreply.github.com>
2022-09-15 12:15:26 -0300

    feat - add motivo_contatos in database, and new method's in controllers

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-
-        var_dump($_POST);
+    public function contato(Request $request) {
+/*
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+*/
+            //$contato = new SiteContato();
+            //$contato->create($request->all());
+
+    //simulando conteudo do banco e enviando esse array para a view site.contato
+        $motivo_contatos = MotivoContato::all();
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request)
+    {
+
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            'telefone' => 'required',
+            'email' => 'required|email|unique:site_contatos',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000'
+        ];
+
+        $feedback = [
+            'nome.required' => 'O campo nome precisa ser preenchido',
+            'nome.min' => 'O nome precisa de pelomenos 3 caracteres',
+            'nome.max' => 'O nome não pode ter mais que 40 caracteres',
+            'nome.unique' => 'O nome ja existe',
+
+            'email.email' => 'O email informado não é válido',
+            'mensagem.max' => 'A mensagem não pode passar de 2000 caracteres',
+            'required' => 'o campo :attribute é obrigatório'
+        ];
+            //realizar a validação do formulario recebidos no request
+        $request->validate($regras, $feedback);
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
 use Illuminate\Http\Request;
 
 class PrincipalController extends Controller
 {
     public function principal() {
-        return view('site.principal');
+        //enviando o parametro $motivo_contatos para view
+        $motivo_contatos = MotivoContato::all();
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
 }
