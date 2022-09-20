c5df2e191bc63f00ecc71e748b90a808014032d9
Cales <86434489+calesj@users.noreply.github.com>
2022-09-20 13:08:25 -0300

    feat - add layouts: produto

---
+++ resources/views/app/produto.blade.php
@@ -0,0 +1,9 @@
+@extends('app.layouts.basico')
+
+<!-- Enviando a palavra 'Home' como parâmetro titulo para o 'basico.blade.php'-->
+@section('titulo', 'Produto')
+
+<!-- Enviando o conteudo inteiro abaixo como parâmetro 'conteudo' para o 'basico.blade.php'-->
+@section('conteudo')
+    <br><br><br><br>Produto
+@endsection
