6b42199b7b9db4babf5dc285b5b2af9fc52cc56d
Cales <86434489+calesj@users.noreply.github.com>
2022-09-21 09:30:04 -0300

    feat - move fornecedor.blade.php to fornecedor directory, rename fornecedor.blade.php to index

--- resources/views/app/fornecedor.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app.layouts.basico')
-
-<!-- Enviando a palavra 'Home' como parâmetro titulo para o 'basico.blade.php'-->
-@section('titulo', 'Fornecedor')
-
-<!-- Enviando o conteudo inteiro abaixo como parâmetro 'conteudo' para o 'basico.blade.php'-->
-@section('conteudo')
-    <br><br><br><br>Fornecedor
-@endsection
---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,31 @@
+@extends('app.layouts.basico')
+
+<!-- Enviando a palavra 'Home' como parâmetro titulo para o 'basico.blade.php'-->
+@section('titulo', 'Fornecedor')
+
+<!-- Enviando o conteudo inteiro abaixo como parâmetro 'conteudo' para o 'basico.blade.php'-->
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Fornecedor</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="">Novo</a> </li>
+                <li><a href="">Consulta</a> </li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                <form method="post" action="">
+                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
+                    <input type="text" name="site" placeholder="Site" class="borda-preta">
+                    <input type="text" name="uf" placeholder="UF" class="borda-preta">
+                    <input type="text" name="email" placeholder="Email" class="borda-preta">
+                    <button type="submit" class="borda-preta">Pesquisar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
