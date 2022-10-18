77da5e5145b79e501e6b9bdede0c4314dd6c1ddc
Cales <86434489+calesj@users.noreply.github.com>
2022-10-18 10:06:17 -0300

    Update AutenticadorMiddleware.php

--- app/Http/Middleware/AutenticadorMiddleware.php
+++ app/Http/Middleware/AutenticadorMiddleware.php
@@ -16,7 +16,10 @@ class AutenticadorMiddleware
     public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
         session_start();
+        //Verifica se os campos não estão vazios
         if(isset($_SESSION['email']) && $_SESSION['email'] != ''){
+            //se não estiver, ele libera o request para o proximo middleware
+            //se não tiver proximo middleware, ele libera o request para o controller
             return $next($request);
         } else {
             return redirect()->route('site.login',['erro' => 2]);
