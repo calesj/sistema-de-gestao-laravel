206af43b94149f92fcaa50c4d43acf7a60c3637d
Cales <cales.junior2002@gmail.com>
2022-10-18 08:58:37 -0300

    feat - add remove option in pedido-produto

--- resources/views/app/pedido-produto/create.blade.php
+++ resources/views/app/pedido-produto/create.blade.php
@@ -22,6 +22,34 @@
             <p>ID do pedido: {{ $pedido->id }}</p>
             <p>Cliente: {{$pedido->cliente_id}}</p>
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                <h4>Itens do pedido</h4>
+                <table border="1" width="100%">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome do Produto</th>
+                        <th>Data de inclusão do item no pedido</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($pedido->produtos as $produto)
+                        <tr>
+                            <td>{{$produto->id}}</td>
+                            <td>{{$produto->nome}}</td>
+                            <td>{{$produto->pivot->created_at->format('d/m/Y')}}</td>
+                            <td>
+                                <form id="form_{{$produto->pivot->id}}" method="post"
+                                      action="{{ route('pedido-produto.destroy', ['pedidoProduto' => $produto->pivot->id, 'pedido_id' => $pedido->id]) }}">
+                                    @method('delete')
+                                    @csrf
+                                    <a href="#" onclick="document.getElementById('form_{{$produto->pivot->id}}').submit()">Excluir</a>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
                 @component('app.pedido-produto._components.form_create',['pedido' => $pedido, 'produtos' => $produtos])
                 @endcomponent
             </div>
